e0c2f1682b8f1dcd6e66730367f08a1b81fcecbd
D Sunder Raj <dsunderraj37@gmail.com>
2024-08-22 03:16:02 +0530

    props and managing states of the news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem;" }}>
-          <img
-            src="https://ichef.bbci.co.uk/news/1024/branded_sport/3ade/live/d66fee50-5ec6-11ef-b43e-6916dcba5cbf.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetails" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetails" className="btn btn-sm btn-primary">
+              ReadMore
             </a>
           </div>
         </div>
